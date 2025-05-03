2003ed2f3670145f84cf0051c0f8f7f1b73144f8
kmbzn <kmbzn@example.com>
2025-05-03 22:55:36 +0900

    logo centre

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,16 +1,18 @@
 <template>
-  <ParentLayout />
+  <div class="theme-container">
+    <Navbar />
+    <ParentLayout />
+  </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
 import ParentLayout from '@parent-theme/layouts/Layout.vue'
 
 export default {
   components: {
+    Navbar,
     ParentLayout
   }
 }
 </script>
-
-<style>
-</style>
