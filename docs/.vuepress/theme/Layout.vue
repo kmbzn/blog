5b7573d07924c1165ae4c44c3742ec8fc5c4ac83
kmbzn <kmbzn@example.com>
2025-05-03 23:01:41 +0900

    error

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="theme-container">
-    <Navbar />
-    <ParentLayout />
-  </div>
+  <ParentLayout />
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
 import ParentLayout from '@parent-theme/layouts/Layout.vue'
 
 export default {
   components: {
-    Navbar,
     ParentLayout
   }
 }
 </script>
+
+<style>
+</style>
